d05772322c552dc7bff794a27f27cdc91a98dd38
fengkun <fengkun666@126.com>
2016-03-08 13:56:12 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ app/src/main/java/com/coolweather/app/model/City.java
@@ -0,0 +1,43 @@
+package com.coolweather.app.model;
+
+/**
+ * Created by fengkun on 2016/3/8.
+ */
+public class City {
+    private  int id;
+    private String cityName;
+    private String cityCode;
+    private int provinceId;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+}
